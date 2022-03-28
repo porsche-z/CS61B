bd0c39124574aea65a8eaeba9fd9e6f8a7373d6c
ZHY <1693697214@qq.com>
2022-03-28 19:50:51 +0800

    Project2's pre lab has been finished

--- skeleton-sp18/proj2/byog/lab5/BoringWorldDemo.java
+++ skeleton-sp18/proj2/byog/lab5/BoringWorldDemo.java
@@ -18,6 +18,8 @@ public class BoringWorldDemo {
 
         // initialize tiles
         TETile[][] world = new TETile[WIDTH][HEIGHT];
+        System.out.println(world.length);
+
         for (int x = 0; x < WIDTH; x += 1) {
             for (int y = 0; y < HEIGHT; y += 1) {
                 world[x][y] = Tileset.NOTHING;
@@ -27,7 +29,7 @@ public class BoringWorldDemo {
         // fills in a block 14 tiles wide by 4 tiles tall
         for (int x = 20; x < 35; x += 1) {
             for (int y = 5; y < 10; y += 1) {
-                world[x][y] = Tileset.WALL;
+                world[x][y] = Tileset.GRASS;
             }
         }
 
